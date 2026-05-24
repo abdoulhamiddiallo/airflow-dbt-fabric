2f0b33b84ad466febdb54bd9935c236bb780a55e
Abdoul Hamid Diallo <AbdoulHamidDIALLO@AHDTechSolutions.onmicrosoft.com>
2026-05-24 05:01:17 +0200

    add modification

---
+++ dags/nyc_taxi_green/target/run/nyc_taxi_green/models/nyc_trip_count.sql
@@ -0,0 +1,42 @@
+
+  
+    
+    
+    USE [taxi];
+    
+    
+
+    EXEC('create view [dbo].[nyc_trip_count__dbt_temp__dbt_tmp_vw] as with new_york_taxis as (
+       select * from nyctlc
+   ),
+   final as (
+     SELECT
+       vendorID,
+       CAST(lpepPickupDatetime AS DATE) AS trip_date,
+       COUNT(*) AS trip_count
+     FROM
+         [taxi].[dbo].[nyctlc]
+     GROUP BY
+         vendorID,
+         CAST(lpepPickupDatetime AS DATE)
+   
+   )
+   select * from final;');
+
+
+
+
+    
+    
+
+    
+    
+            EXEC('CREATE TABLE [taxi].[dbo].[nyc_trip_count__dbt_temp]  AS SELECT * FROM [taxi].[dbo].[nyc_trip_count__dbt_temp__dbt_tmp_vw] 
+    OPTION (LABEL = ''dbt-fabric-dw'');
+');
+        
+    
+
+
+  
+  
